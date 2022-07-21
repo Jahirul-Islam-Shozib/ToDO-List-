377ac1457ae803ad759b8b9eca0ff57d20e1ad66
unknown <mshozib000@gmail.com>
2022-07-21 13:28:43 +0600

    first commit

---
+++ src/app/Todo.ts
@@ -0,0 +1,13 @@
+export class Todo {
+  public sno: number;
+  public title: string;
+  public desc: string;
+  public active: boolean;
+
+  constructor(sno: number, title: string, desc: string, active: boolean) {
+    this.sno = sno;
+    this.title = title;
+    this.desc = desc;
+    this.active = active;
+  }
+}
